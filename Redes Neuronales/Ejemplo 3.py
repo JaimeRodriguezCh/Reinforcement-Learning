408f8e873a1c95b119e32ec00da7eaa45278c6d2
JaimeRodriguezCh <jaime.rodriguezchala@gmail.com>
2023-09-21 15:14:06 -0300

    RNA

--- Redes Neuronales/Ejemplo 3.py
+++ Redes Neuronales/Ejemplo 3.py
@@ -1,42 +1,27 @@
 
-'''
+
 from math import exp
 import sympy as sp
 from sympy.utilities import lambdify
+import matplotlib.pyplot as plt
+## Conjunto T y parametros
+x11, x12, x21, x22, x31, x32, x41, x42, y1, y2, y3, y4, n, W1, W2, B, eta, List, c, P = 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 4, -1, -1, 1, 0.001, [], 0, 1
 
-x11=0
-x12=0
-x21=0
-x22=1
-x31=1
-x32=0
-x41=1
-x42=1
-y1=1
-y2=1
-y3=1
-y4=0
-n=4
-
+# Definicion de Funcion C
 w1, w2, b= sp.symbols('w1 w2 b')
-
 C = (1/(2*n))*(((1 / (1 + sp.exp(-(w1*x11+w2*x12+b))))-y1)**2+((1 / (1 + sp.exp(-(w1*x21+w2*x22+b))))-y2)**2+((1 / (1 + sp.exp(-(w1*x31+w2*x32+b))))-y3)**2+((1 / (1 + sp.exp(-(w1*x41+w2*x42+b))))-y4)**2)
 C_eval = lambdify((w1,w2,b), C, "numpy")
 
+#Definicion de sus derivadas
 derivada_w1 = sp.diff(C, w1)
 derivada_w1_eval = lambdify((w1,w2,b), derivada_w1, "numpy")
 derivada_w2 = sp.diff(C, w2)
 derivada_w2_eval = lambdify((w1,w2,b), derivada_w2, "numpy")
 derivada_b = sp.diff(C, b)
 derivada_b_eval = lambdify((w1,w2,b), derivada_b, "numpy")
-W1 = -1
-W2=-1
-B=1
-eta=0.001
 
-List=[]
-c=0
-P=1
+
+# Algoritmo de descenso del gradiente con criterio de parada C_T(w,b)<0.01
 while C_eval(W1,W2,B)>0.01 :
     if c% P == 0:
         print(c)
@@ -46,25 +31,20 @@ while C_eval(W1,W2,B)>0.01 :
     W2=W2-eta*(derivada_w2_eval(W1,W2,B))
     B=B-eta*(derivada_b_eval(W1,W2,B))
     c=c+1
-print(List)  
-print(c)  
 
-   
-
-
+#Resultados
 print(C_eval(W1,W2,B))    
 print(W1,W2,B)
 print((1 / (1 + exp(-(W1*x11+W2*x12+B)))))
 print((1 / (1 + exp(-(W1*x21+W2*x22+B)))))
 print((1 / (1 + exp(-(W1*x31+W2*x32+B)))))
 print((1 / (1 + exp(-(W1*x41+W2*x42+B)))))
-'''
 
-import matplotlib.pyplot as plt
 
-# Datos
+
+# Grafico ilustrativo que muestra como la función de error decrementa a medida que pasan iteraciones.
 x = [0, 10, 100, 1000, 10000, 100000, 588039]
-y = [0.08058237203212831, 0.08053684779395877, 0.08013086796426971, 0.07642348344611484, 0.05977434683869173, 0.03525470740933883, 0.009999989551141186]
+y = List
 
 # Crear el gráfico de líneas
 plt.plot(x, y, label='Datos de Error', color='blue', marker='o', linestyle='-')
@@ -74,7 +54,7 @@ plt.xlabel('Iteraciones')
 plt.ylabel('Valor de la función de error')
 
 # Título del gráfico
-plt.title('Gráfico de Error vs. Iteraciones con Líneas Rectas')
+plt.title('Gráfico de Error vs. Iteraciones')
 
 # Mostrar la leyenda
 plt.legend()
